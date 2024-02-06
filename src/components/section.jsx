1ec5d30d90b9c80cb8942bd2efff70f267a4eb99
Sajjad Saber <sajjad.saberi1996@gmail.com>
2024-02-06 20:03:02 +0330

    add contact and titles

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -7,12 +7,40 @@ function Sections() {
         <main className='flex justify-between items-center mt-6'>
             <div className='bg-slate-200 w-60 h-96 rounded-md shadow-11xxxl'>
                 <p className=' bg-stone-800 text-white h-12 pt-3 rounded-t-md rounded-b-none font-Iran'>فهرست ها</p>
+                <div className='font-Iran flex justify-end items-center w-auto h-auto mt-5 mr-5'>
+                    <ul>
+                        <li>
+                            <a href='' >سر عنوان</a>
+                            <ul>
+                                <li>عنوان</li>
+                                <li>عنوان</li>
+                                <li>عنوان</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href='' >سر عنوان</a>
+                            <ul>
+                                <li>عنوان</li>
+                                <li>عنوان</li>
+                                <li>عنوان</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href='' >سر عنوان</a>
+                            <ul>
+                                <li>عنوان</li>
+                                <li>عنوان</li>
+                                <li>عنوان</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div className='bg-slate-200 w-550xl h-96 rounded-md shadow-11xxxl'>
-                <p className=' bg-stone-800 text-white h-12 pt-3 rounded-t-md rounded-b-none font-Iran'>فهرست ها</p>
+                <p className=' bg-stone-800 text-white h-12 pt-3 rounded-t-md rounded-b-none font-Iran'>عنوان پست های انتشار داده شده در وبسایت</p>
             </div>
             <div className=' bg-slate-200 w-60 h-96 rounded-md shadow-11xxxl'>
-                <p className=' bg-stone-800 text-white h-12 pt-3 rounded-t-md rounded-b-none font-Iran'>فهرست ها</p>
+                <p className=' bg-stone-800 text-white h-12 pt-3 rounded-t-md rounded-b-none font-Iran'>برترین ها</p>
             </div>
         </main>
         </>
