9eede4540de2563c57bae06cacf709520cdd0c10
Sajjad Saber <sajjad.saberi1996@gmail.com>
2024-02-06 19:51:45 +0330

    first update to sections and mains

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -6,13 +6,13 @@ function Sections() {
         <> 
         <main className='flex justify-between items-center mt-6'>
             <div className='bg-slate-200 w-60 h-96 rounded-md shadow-11xxxl'>
-                <p>فهرست ها</p>
+                <p className=' bg-stone-800 text-white h-12 pt-3 rounded-t-md rounded-b-none font-Iran'>فهرست ها</p>
             </div>
             <div className='bg-slate-200 w-550xl h-96 rounded-md shadow-11xxxl'>
-                <p>فهرست ها</p>
+                <p className=' bg-stone-800 text-white h-12 pt-3 rounded-t-md rounded-b-none font-Iran'>فهرست ها</p>
             </div>
             <div className=' bg-slate-200 w-60 h-96 rounded-md shadow-11xxxl'>
-                <p>فهرست ها</p>
+                <p className=' bg-stone-800 text-white h-12 pt-3 rounded-t-md rounded-b-none font-Iran'>فهرست ها</p>
             </div>
         </main>
         </>
