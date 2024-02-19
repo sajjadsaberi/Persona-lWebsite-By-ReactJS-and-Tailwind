4b7647f37d1b620bb3649a739f7df4a6249855c2
Sajjad Saber <sajjad.saberi1996@gmail.com>
2024-02-19 19:43:32 +0330

    completed the Sections and Mains

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ function Footers() {
         <> 
         <footer className='flex justify-between items-center flex-row w-full h-auto bg-gray-900 mt-9 p-6 font-Iran text-white rtl'>
             <div className='w-40'>
-                <a href="#" className='text-yellow-400 border-b-2 border-white pb-2.5 drop-shadow-dx10'>لینک های مرتبط</a>
+                <a href="#" className='text-yellow-400 border-b-2 hover:text-yellow-400 border-white pb-2.5 drop-shadow-dx10'>لینک های مرتبط</a>
                 <ul className="list-disc rtl list-inside mt-3">
                     <li className='pt-1.5'><a href="#" className='text-white hover:text-slate-500'>سایت های شخصی</a></li>
                     <li className='pt-1.5'><a href="#" className='text-white hover:text-slate-500'>سایت های شخصی</a></li>
@@ -15,16 +15,16 @@ function Footers() {
                 </ul>
             </div>
             <div className='w-40'>
-                <a href='#' className='text-yellow-400 border-b-2 border-white pb-2.5 drop-shadow-dx10'>فهرست سایت</a>
+                <a href='#' className='text-yellow-400 hover:text-yellow-400 border-b-2 border-white pb-2.5 drop-shadow-dx10'>فهرست سایت</a>
                 <ul className=" list-disc rtl list-inside mt-3">
-                    <li className='pt-1.5'>لینک های ویژه</li>
-                    <li className='pt-1.5'>لینک های ویژه</li>
-                    <li className='pt-1.5'>لینک های ویژه</li>
-                    <li className='pt-1.5'>لینک های ویژه</li>
+                    <li className='pt-1.5'><a href="#" className='text-white hover:text-slate-500'>لینک های ویژه</a></li>
+                    <li className='pt-1.5'><a href="#" className='text-white hover:text-slate-500'>لینک های ویژه</a></li>
+                    <li className='pt-1.5'><a href="#" className='text-white hover:text-slate-500'>لینک های ویژه</a></li>
+                    <li className='pt-1.5'><a href="#" className='text-white hover:text-slate-500'>لینک های ویژه</a></li>
                 </ul>
             </div>
             <div className='w-350xl h-40'>
-                <a href='#' className='text-yellow-400 border-b-2 border-white pb-2.5 drop-shadow-dx10'>درباره ما</a>
+                <a href='#' className='text-yellow-400 hover:text-yellow-400 border-b-2 border-white pb-2.5 drop-shadow-dx10'>درباره ما</a>
                 <p className='text-justify mt-5 leading-9'>
                     در این قسمت یک متن کوتاه در خصوص نحوه راه اندازی این وبسایت و بیوگرافی شخصی سجاد صابری قرار داده می شود و کاربر می توان از طریق آن با با ما آشنا شود.
                 </p>
