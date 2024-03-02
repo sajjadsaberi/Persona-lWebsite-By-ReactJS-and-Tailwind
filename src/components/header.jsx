f6ef16c2a8ca7a9d3bd79eefb8c76102b3d69c86
Sajjad Saber <sajjad.saberi1996@gmail.com>
2024-03-02 19:38:51 +0330

    add another jsx Files

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,10 @@
 import {React} from 'react'
 import './header.module.css'
 
+const me = () => {
+    
+};
+
 function Headers() {
     return (
         <> 
@@ -28,7 +32,7 @@ function Headers() {
         <img src='./src/images/Header.jpg' alt='Header' className='h-72 w-10xl rounded-t-md'/>
     </header>
     <nav className='bg-gray-900 flex items-center justify-between gap-96 shadow-5xxl rounded-t-none rounded-b-md'>
-        <span className='text-gray-950 pt-0 pb-8 pl-3.5 font-Iran text-sm'><img className='absolute z-20 mt-1.5 ml-1.5 cursor-grab' src='./src/images/Search.png' width="20px" height="20px" alt='Search'/><input type='text' placeholder='جست و جو ...' className='absolute w-56 h-8 outline-none rtl pr-1 z-10 rounded-md' required /></span>
+        <span className='text-gray-950 pt-0 pb-8 pl-3.5 font-Iran text-sm'><img className='absolute z-20 mt-1.5 ml-1.5 cursor-grab' src='./src/images/Search.png' width="20px" height="20px" alt='Search'/><input onClick={me()} type='text' placeholder='جست و جو ...' className='absolute w-56 h-8 outline-none rtl pr-1 z-10 rounded-md' required /></span>
         <ul className='list-none p-2 m-0 flex'>
             <li className='mr-5 transition ease-in-out delay-150 hover:bg-gray-800  hover:transition hover:ease-in-out hover:delay-150'><a href="#" className='inline-block text-white text-center py-2 px-2 no-underline font-Iran cursor-pointer hover:text-white'>ارتباط با ما</a></li>
             <li className='mr-5 transition ease-in-out delay-150 hover:bg-gray-800  hover:transition hover:ease-in-out hover:delay-150'><a href="#" className='inline-block text-white text-center py-2 px-2 no-underline font-Iran cursor-pointer hover:text-white'>درباره ما</a></li>
---
+++ src/components/test.jsx
@@ -0,0 +1,10 @@
+import {React} from 'react';
+
+function Tests() {
+    return (
+        <>  
+        </>
+    )
+}
+
+export default Tests ; 
